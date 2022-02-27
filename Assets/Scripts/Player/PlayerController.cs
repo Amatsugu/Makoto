8200a71058f63318d77503126b05cc4fe41a038b
Amatsugu <khamraj@kaisei.app>
2022-02-26 19:51:47 -0500

    Handle movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+	public float moveSpeed;
+	public float turnSpeed;
+
 	private PlayerInput _input;
+	private CharacterController _characterController;
+	private Vector2 _vel;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		_input = GetComponent<PlayerInput>();
+		_characterController = GetComponent<CharacterController>();
+		_vel = Vector3.zero;
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
@@ -26,13 +34,32 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
+		_characterController.Move(_vel * Time.deltaTime);
 	}
 
 	public void Move(InputAction.CallbackContext context)
 	{
-		var pos = context.ReadValue<Vector2>();
-		Debug.Log($"Move: {pos}");
+		switch (context.phase)
+		{
+			case InputActionPhase.Disabled:
+				break;
+			case InputActionPhase.Waiting:
+				break;
+			case InputActionPhase.Started:
+				break;
+			case InputActionPhase.Performed:
+				var pos = context.ReadValue<Vector2>();
+				_vel = pos * moveSpeed;
+				break;
+			case InputActionPhase.Canceled:
+				_vel = Vector2.zero;
+				break;
+		}
+	}
+
+	public void Dash(InputAction.CallbackContext context)
+	{
+
 	}
 
 	public void Aim(InputAction.CallbackContext context)
