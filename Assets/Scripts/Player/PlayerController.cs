60609a4d9e33460300e82a30ef6659a04bcac9fd
Amatsugu <khamraj@kaisei.app>
2022-03-07 21:36:26 -0500

    Finalzied controls config Fixed collision Added look rotation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 	public float dashDistance;
 	public float turnSpeed;
 
+	public Transform body;
+
 	private PlayerInput _input;
 	private Rigidbody2D _rigidbody;
 	private Vector2 _vel;
@@ -37,18 +39,22 @@ public class PlayerController : MonoBehaviour
 
 #endif
 
+	public void Update()
+	{
+		body.rotation = Quaternion.LookRotation(Vector3.forward, _curLook);
+		Debug.DrawRay(_transform.position, _curLook, Color.magenta);
+	}
+
 	// Update is called once per frame
-	private void Update()
+	private void FixedUpdate()
 	{
 		var curVel = _vel;
 		if (_dashTime > Time.time)
 		{
 			curVel = _dashVel * dashSpeed;
 			Debug.DrawRay(_transform.position, curVel.normalized * dashDistance, Color.white);
-		}
-		/*_characterController.Move(curVel * Time.deltaTime);*/
+		}	
 		_rigidbody.velocity = curVel;
-		Debug.DrawRay(_transform.position, _curLook, Color.magenta);
 	}
 
 	public void Move(InputAction.CallbackContext context)
@@ -87,6 +93,8 @@ public class PlayerController : MonoBehaviour
 
 	public void Aim(InputAction.CallbackContext context)
 	{
+		if (context.phase == InputActionPhase.Canceled)
+			return;
 		var look = context.ReadValue<Vector2>();
 		_curLook = look;
 		Debug.Log($"Aim: {look}");
